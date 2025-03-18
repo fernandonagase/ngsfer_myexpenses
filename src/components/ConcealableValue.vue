ada9652c2c8a2d794b17aa3934acb289aee7489b
Fernando Nagase <nagase.fernando@gmail.com>
2025-03-17 21:02:40 -0300

    ajustar novo padrão ordem SFC

--- src/components/ConcealableValue.vue
+++ src/components/ConcealableValue.vue
@@ -1,12 +1,12 @@
+<script setup lang="ts">
+import { useConfigStore } from 'src/stores/config-store'
+
+const configStore = useConfigStore()
+</script>
+
 <template>
   <template v-if="!configStore.hideValues">
     <slot></slot>
   </template>
   <span v-else>***</span>
 </template>
-
-<script setup lang="ts">
-import { useConfigStore } from 'src/stores/config-store'
-
-const configStore = useConfigStore()
-</script>
