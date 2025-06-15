9a88ae7476c47aa6178114255162102af50b21a7
Fernando Nagase <nagase.fernando@gmail.com>
2025-06-15 13:05:27 -0300

    adicionar um espaço após o R$ ao formatar moeda

--- src/helpers/currency.js
+++ src/helpers/currency.js
@@ -1,5 +1,5 @@
 import currency from 'currency.js'
 
-const BRL = (value) => currency(value, { symbol: 'R$', separator: '.', decimal: ',' })
+const BRL = (value) => currency(value, { symbol: 'R$ ', separator: '.', decimal: ',' })
 
 export { BRL }
