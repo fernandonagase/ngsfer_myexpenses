ecfc33d90bdf804860828542bc11814d41ba3489
Fernando Nagase <nagase.fernando@gmail.com>
2026-02-24 20:04:05 -0300

    Salvar opção de ocultar valores no local storage

--- src/stores/config-store.ts
+++ src/stores/config-store.ts
@@ -2,10 +2,25 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useConfigStore = defineStore('config', () => {
-  const hideValues = ref(true)
+  const HIDE_VALUES_STORAGE_KEY = 'config.hideValues'
+
+  function getInitialHideValues() {
+    const savedValue = localStorage.getItem(HIDE_VALUES_STORAGE_KEY)
+    if (savedValue === null) {
+      return true
+    }
+    return savedValue === 'true'
+  }
+
+  const hideValues = ref(getInitialHideValues())
+
+  function persistHideValues() {
+    localStorage.setItem(HIDE_VALUES_STORAGE_KEY, String(hideValues.value))
+  }
 
   function toggleValuesVisibility() {
     hideValues.value = !hideValues.value
+    persistHideValues()
   }
 
   return { hideValues, toggleValuesVisibility }
