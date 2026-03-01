a456236427b4b8506f34fdca78a34ee1ce728c1e
Fernando Nagase <nagase.fernando@gmail.com>
2026-03-01 12:16:55 -0300

    Adicionar suporte para a web

--- src/boot/typeorm.ts
+++ src/boot/typeorm.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata'
 import { defineBoot } from '#q-app/wrappers'
+import { JeepSqlite } from 'jeep-sqlite/dist/components/jeep-sqlite'
 
 import expensesDataSource from 'src/databases/datasources/ExpensesDatasource'
 import sqliteParams from 'src/databases/sqliteParams'
+
+customElements.define('jeep-sqlite', JeepSqlite)
 
 const initializeDataSources = async () => {
   //check sqlite connections consistency
@@ -25,5 +28,25 @@ const initializeDataSources = async () => {
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli-vite/boot-files
 export default defineBoot(async (/* { app, router, ... } */) => {
-  await initializeDataSources()
+  if (sqliteParams.platform !== 'web') {
+    try {
+      await initializeDataSources()
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+  } else {
+    const jeepEl = document.createElement('jeep-sqlite')
+    document.body.appendChild(jeepEl)
+    await customElements
+      .whenDefined('jeep-sqlite')
+      .then(async () => {
+        await sqliteParams.connection.initWebStore()
+        await initializeDataSources()
+      })
+      .catch((err) => {
+        console.error(`Error: ${err}`)
+        throw new Error(`Error: ${err}`)
+      })
+  }
 })
