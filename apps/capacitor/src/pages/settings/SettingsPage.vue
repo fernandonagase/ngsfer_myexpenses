09756fb837acd5c2485e35125c2163f8a22d911f
Fernando Nagase <nagase.fernando@gmail.com>
2026-03-19 12:45:35 -0300

    Adicionar opções na página de configurações

--- apps/capacitor/src/pages/settings/SettingsPage.vue
+++ apps/capacitor/src/pages/settings/SettingsPage.vue
@@ -4,10 +4,16 @@ import { useQuasar } from 'quasar'
 import ErrorBoundary from 'src/components/ErrorBoundary.vue'
 import { actWithDbConnectionStopped } from 'src/databases/datasources/ExpensesDatasource'
 import { backup, importBackup } from 'src/services/backup-service'
+import { useCategoryStore } from 'src/stores/category-store'
+import { useCenterStore } from 'src/stores/center-store'
 import { useOperationStore } from 'src/stores/operation-store'
 
 const $q = useQuasar()
 const { refreshScreen } = useOperationStore()
+
+const centerStore = useCenterStore()
+const categoryStore = useCategoryStore()
+// const operationStore = useOperationStore()
 
 async function doBackup() {
   $q.loading.show({
@@ -49,6 +55,33 @@ async function doImportBackup() {
   <q-page>
     <ErrorBoundary>
       <q-list>
+        <q-item-label header>Cadastros</q-item-label>
+        <q-item clickable v-ripple @click="centerStore.showCenters()">
+          <q-item-section avatar>
+            <q-icon name="account_balance" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Centros financeiros</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple @click="categoryStore.showCategories()">
+          <q-item-section avatar>
+            <q-icon name="label" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Categorias</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple :to="{ name: 'recurrence' }">
+          <q-item-section avatar>
+            <q-icon name="event_repeat" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Operações recorrentes</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-separator spaced />
+        <q-item-label header>Backup</q-item-label>
         <q-item clickable v-ripple @click="doImportBackup()">
           <q-item-section avatar>
             <q-icon name="cloud_download" />
