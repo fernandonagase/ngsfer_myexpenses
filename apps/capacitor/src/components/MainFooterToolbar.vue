6eb463f22da41770ed87ad77bf6d7dd11e30bf05
Fernando Nagase <nagase.fernando@gmail.com>
2026-03-19 13:29:16 -0300

    Extrair componente MainFooterToolbar

---
+++ apps/capacitor/src/components/MainFooterToolbar.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { useOperationStore } from 'src/stores/operation-store'
+
+const operationStore = useOperationStore()
+</script>
+
+<template>
+  <q-toolbar class="bg-white text-dark main-footer-toolbar q-px-xs">
+    <div class="col row justify-center">
+      <q-btn
+        stack
+        flat
+        no-caps
+        label="Início"
+        icon="home"
+        color="primary"
+        class="col-6"
+        :to="{ name: 'operations' }"
+      />
+    </div>
+    <div class="fab-space"></div>
+    <div class="col row justify-center">
+      <q-btn
+        stack
+        flat
+        no-caps
+        label="Relatório"
+        icon="trending_up"
+        color="primary"
+        class="col-6"
+        :to="{ name: 'operations-by-category' }"
+      />
+      <q-btn
+        stack
+        flat
+        no-caps
+        label="Mais"
+        icon="menu"
+        color="primary"
+        class="col-6"
+        :to="{ name: 'settings' }"
+      />
+    </div>
+    <q-btn
+      class="main-footer-add"
+      fab
+      icon="add"
+      color="primary"
+      @click="operationStore.addOperation()"
+    />
+  </q-toolbar>
+</template>
+
+<style scoped lang="scss">
+.main-footer-toolbar {
+  border-top: 1px solid $grey-4;
+  position: relative;
+  overflow: visible;
+}
+
+.main-footer-add {
+  position: absolute !important;
+  left: 50% !important;
+  top: -20px !important;
+  transform: translateX(-50%) !important;
+  z-index: 10;
+}
+
+.fab-space {
+  width: 56px;
+}
+</style>
--- apps/capacitor/src/layouts/CenterLayout.vue
+++ apps/capacitor/src/layouts/CenterLayout.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import CenterToolbar from 'src/components/center/CenterToolbar.vue'
-import { useOperationStore } from 'src/stores/operation-store';
-
-const operationStore = useOperationStore()
+import MainFooterToolbar from 'src/components/MainFooterToolbar.vue';
 </script>
 
 <template>
@@ -17,39 +15,10 @@ const operationStore = useOperationStore()
       </q-page-container>
 
       <q-footer>
-        <q-toolbar class="bg-white text-dark main-footer-toolbar q-px-xs">
-          <div class="col row justify-center">
-            <q-btn stack flat no-caps label="Início" icon="home" color="primary" class="col-6" :to="{ name: 'operations' }" />
-          </div>
-          <div class="fab-space"></div>
-          <div class="col row justify-center">
-            <q-btn stack flat no-caps label="Relatório" icon="trending_up" color="primary" class="col-6" :to="{ name: 'operations-by-category' }" />
-            <q-btn stack flat no-caps label="Mais" icon="menu" color="primary" class="col-6" :to="{ name: 'settings' }" />
-          </div>
-          <q-btn class="main-footer-add" fab icon="add" color="primary" @click="operationStore.addOperation()" />
-        </q-toolbar>
+        <MainFooterToolbar />
       </q-footer>
     </q-layout>
     <template #fallback> Carregando... </template>
   </Suspense>
 </template>
 
-<style scoped lang="scss">
-.main-footer-toolbar {
-  border-top: 1px solid $grey-4;
-  position: relative;
-  overflow: visible;
-}
-
-.main-footer-add {
-  position: absolute !important;
-  left: 50% !important;
-  top: -20px !important;
-  transform: translateX(-50%) !important;
-  z-index: 10;
-}
-
-.fab-space {
-  width: 56px;
-}
-</style>
